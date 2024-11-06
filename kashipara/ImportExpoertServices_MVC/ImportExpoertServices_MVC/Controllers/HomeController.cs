cbba9e052cbe8cbb7a373c18d173152a5296d463
onlycaffeine <60136864+onlycaffeine@users.noreply.github.com>
2024-11-06 16:27:11 +0700

    up

---
+++ kashipara/ImportExpoertServices_MVC/ImportExpoertServices_MVC/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.Entity;
+using ImportExpoertServices_MVC.Models;
+
+
+namespace ImportExpoertServices_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+
+        }
+    }
+}
---
+++ kashipara/ImportExpoertServices_MVC/ImportExpoertServices_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ImportExpoertServices_MVC.MvcApplication" Language="C#" %>
